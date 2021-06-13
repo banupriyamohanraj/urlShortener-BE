6ee3d4cdf029267555a687b85f205a1de5388e02
banupriyamohanraj <banupriyamohanrajoffl@gmail.com>
2021-06-13 23:25:07 +0530

    short url commit

--- url.js
+++ url.js
@@ -1,4 +1,4 @@
-require ("dotenv").config();
+require("dotenv").config();
 const router = require('express').Router();
 const { MongoClient, ObjectID } = require('mongodb')
 const cors = require('cors')
@@ -13,62 +13,70 @@ const shortid = require('shortid');
 const dbURL = process.env.DB_URL || 'mongodb://127.0.0.1:27017'
 
 
-router.post('/createurl',async (req,res)=>{
-  
-  try {
-    let client = await MongoClient.connect(dbURL);
-    let db = await client.db('url');
-    let url = req.body.longurl;
-    console.log(url)
-    if(validUrl.isUri(url)){
-    
-      let short_id = shortid.generate();
-     
-      await db.collection("url").insertOne({longurl : req.body.longurl,shortid: short_id});
-      res.status('200').json({message:"Looks like URL"})
-    }else{
-        res.status("401").json({message:"Not an URL"})
-    }
-    client.close();
-  } catch (error) {
-      console.log(error)
-      res.status(500).json({message:"Internal server error"})
-  }
+router.post('/createurl', async (req, res) => {
 
-
-})
-
-router.get('/list',async(req,res)=>{
-   try {
-    let client = await MongoClient.connect(dbURL);
-    let db = await client.db('url');
-let data= await db.collection("url").find().toArray();
-if(data){
-    let count = await db.collection('url').count();
-    res.status(200).json(data);
-}else{
-    res.status(404).json({message:"data not found"})
-}
-   } catch (error) {
-       console.log(error)
-       res.status(500).json({message:"Internal server error"})
-   }
-
-})
-
-router.get('/:urlid',async(req,res)=>{
     try {
         let client = await MongoClient.connect(dbURL);
         let db = await client.db('url');
-    await db.collection("url").findOne({shortid : req.params.urlid},function(err,data){
-        if(err) throw err;
-        res.redirect(data.longurl)
-    })
- } catch (error) {
+        let url = req.body.longurl;
+        console.log(url)
+        if (validUrl.isUri(url)) {
+
+            let short_id = shortid.generate();
+            let shorturl = `https://urlshortener--be.herokuapp.com/url/${short_id}`
+            await db.collection("url").insertOne({ longurl: req.body.longurl, shortid: shorturl });
+
+
+            res.status('200').json({ message: "Looks like URL" })
+
+        } else {
+            res.status("401").json({ message: "Not an URL" })
+        }
+        client.close();
+    } catch (error) {
         console.log(error)
-        res.status(500).json({message:"Internal server error"})
+        res.status(500).json({ message: "Internal server error" })
     }
-  
+
+
+})
+
+router.get('/list', async (req, res) => {
+    try {
+        let client = await MongoClient.connect(dbURL);
+        let db = await client.db('url');
+        let data = await db.collection("url").find().toArray();
+        if (data) {
+            let count = await db.collection('url').count();
+            res.status(200).json(data);
+        } else {
+            res.status(404).json({ message: "data not found" })
+        }
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ message: "Internal server error" })
+    }
+
+})
+
+router.get('/:urlid', async (req, res) => {
+    try {
+        let client = await MongoClient.connect(dbURL);
+        let db = await client.db('url');
+        await db.collection("url").findOne({ shortid: req.params.urlid }, function (err, data) {
+            if (err) throw err;
+            if (data) {
+                res.redirect(data.longurl)
+            } else {
+                res.status(404).json({ message: "not found" })
+            }
+
+        })
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ message: "Internal server error" })
+    }
+
 })
 
 module.exports = router;
